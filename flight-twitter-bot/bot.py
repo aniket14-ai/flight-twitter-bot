3150fb8d3f2df99e5e7d6dc95a646a6bddb8bf8c
aniket14-ai <aniketoxford14@gmail.com>
2025-05-10 11:18:22 +0530

    Delete flight-twitter-bot directory

--- flight-twitter-bot/bot.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-import tweepy
-
-# Get secrets from environment variables (GitHub Actions will inject these)
-API_KEY = os.getenv("API_KEY")
-API_SECRET = os.getenv("API_SECRET")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_SECRET = os.getenv("ACCESS_SECRET")
